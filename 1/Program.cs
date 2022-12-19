8ac26bd6a16e6059bea9e941f4294f8517acb687
Salavat <malikov.salavat702@gmail.com>
2022-12-20 00:13:25 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход п
+// ятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32 (Console.ReadLine());
+
+string aNum = Convert.ToString(num);
+
+if (aNum[0]==aNum[4]&&aNum[1]==aNum[3])
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
